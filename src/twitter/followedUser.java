b5570c2e09a5fb7a8cfdde172295c24ef3ce9188
Jack Bui <jackbui05@hotmail.com>
2022-11-09 20:02:33 -0800

    to push

---
+++ src/twitter/followedUser.java
@@ -0,0 +1,19 @@
+package twitter; 
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class followedUser{
+	private List<observer> followers = new ArrayList<>(); 
+	
+	
+	public void follow(observer observer) {
+		followers.add(observer); 
+	}
+	
+	public void updateFollowersFeed(String tweet) {
+		for(observer observer : this.followers) {
+			observer.updateFeed(this, tweet);
+		}
+	}
+}
